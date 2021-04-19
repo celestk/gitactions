e2edf9a6405ba049e2856c7ae30bf220ed479d1d
Celeste <celestine88@gmail.com>
2021-04-20 00:47:02 +0800

    Update index.js
    edit code for errors

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const telegram = require('node-telegram-bot-api')
 const bot = new telegram (process.env.TELEGRAM_TOKEN)
 
 
-const getRandomTip = async () =>{
+const getRandomJoke = async () =>{
     var data = require('./jokes.json')
     var obj = JSON.parse(JSON.stringify(data))
     var randomNumber = Math.floor(Math.random() * 68)
